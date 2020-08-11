e7102364e434f67ef28471e2ee4a7678ef1459bf
Gergely Csatari <gergely.csatari@nokia.com>
2020-08-11 14:20:52 +0300

    Adding copyright notices
    Signed-off-by: Gergely Csatari <gergely.csatari@nokia.com>

--- test_cj.c
+++ test_cj.c
@@ -1,3 +1,9 @@
+/*
+ * Copyright 2020 Nokia
+ * Licensed under the BSD 3-Clause License.
+ * SPDX-License-Identifier: BSD-3-Clause
+*/
+
 #include <stdarg.h>
 #include <stddef.h>
 #include <setjmp.h>
